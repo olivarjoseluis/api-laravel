43a096d34992deb309acf3f449dc33db625b1e00
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-03-14 23:31:02 -0500

    V1 redy

---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+use App\Http\Resources\V1\PostResource;
+
+
+class PostController extends Controller
+{
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    return PostResource::collection(Post::latest()->paginate());
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    //
+  }
+
+  /**
+   * Display the specified resource.
+   */
+  public function show(Post $post)
+  {
+    return new PostResource($post);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(Request $request, Post $post)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Post $post)
+  {
+    $title = $post->title;
+    $post->delete();
+
+    return response()->json(["message" => "Post '" . $title . "' has been deleted."], Response::HTTP_NO_CONTENT);
+  }
+}
