5c0cba6b8b4a1a3aa3dd332decc517645c8840c6
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-03-15 01:18:28 -0500

    Token for verification

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
+use App\Http\Controllers\Api\LoginController;
+
 //Version 1
 Route::apiResource('v1/posts', PostV1::class)
   ->only(['index', 'show', 'destroy'])
@@ -15,3 +17,6 @@ Route::apiResource('v1/posts', PostV1::class)
 Route::apiResource('v2/posts', PostV2::class)
   ->only(['index', 'show'])
   ->middleware('auth:sanctum');
+
+
+Route::post('login', [LoginController::class, 'login']);
