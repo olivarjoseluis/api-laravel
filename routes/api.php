5dfad577e3e49327a9ebbe051ae38c6fa0ff389e
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-03-15 00:13:55 -0500

    Middleware for routes

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,11 @@ use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 //Version 1
-Route::apiResource('v1/posts', PostV1::class)->only(['index', 'show', 'destroy']);
+Route::apiResource('v1/posts', PostV1::class)
+  ->only(['index', 'show', 'destroy'])
+  ->middleware('auth:sanctum');
 
 //Version 2
-Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show']);
+Route::apiResource('v2/posts', PostV2::class)
+  ->only(['index', 'show'])
+  ->middleware('auth:sanctum');
